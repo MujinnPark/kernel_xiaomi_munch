c7f42b7c95584e95305f1c3dda04b6dbdc051cdd
MujinnPark <syedamirul008@gmail.com>
2026-07-06 17:06:17 +0800

    fix: property_type micharge telephony

--- PitchKernel/anykernel/patch/pitchkernel_sepolicy.sh
+++ PitchKernel/anykernel/patch/pitchkernel_sepolicy.sh
@@ -1,6 +1,5 @@
 #!/system/bin/sh
 # PitchKernel SELinux policy fixes
-# Each rule is evidence-backed from dmesg AVC denial analysis.
 
 RULES="
 allow vendor_hal_citsensorservice_xiaomi_default vendor_diag_device chr_file { read write open ioctl getattr }
@@ -9,41 +8,38 @@ allow miuibooster platform_app process { getsched setsched }
 allow isolated_app turbosched_service service_manager { find }
 allow vendor_hal_perf_default system_server dir { search }
 allow vendor_hal_perf_default system_server file { read open getattr map }
-allow vendor_hal_perf_default system_server proc { read }
 allow vendor_hal_perf_default ksu file { read open getattr map }
 allow vendor_hal_perf_default ksu dir { search }
+allow vendor_hal_perf_default hal_audio_default dir { search }
 allow vendor_hal_perf_default vendor_hal_perf_default capability { dac_override }
 allow system_server zygote process { setsched }
 allow system_suspend sysfs file { read open getattr }
 allow system_suspend vendor_sysfs_battery_supply file { read open getattr }
-allow zygote vendor_display_prop file { read open getattr map }
-allow platform_app vendor_display_prop file { read open getattr map }
-allow untrusted_app vendor_display_prop file { read open getattr map }
-allow permissioncontroller_app vendor_display_prop file { read open getattr map }
+allow untrusted_app property_type file { read open getattr map }
+allow platform_app property_type file { read open getattr map }
+allow priv_app property_type file { read open getattr map }
+allow system_app property_type file { read open getattr map }
+allow system_app property_type property_service { set }
+allow zygote property_type file { read open getattr map }
+allow mediaprovider_app property_type file { read open getattr map }
+allow permissioncontroller_app property_type file { read open getattr map }
+allow traceur_app property_type file { read open getattr map }
+allow gmscore_app property_type file { read open getattr map }
 allow vendor_init build_prop property_service { set }
 allow vendor_init default_prop property_service { set }
-allow gmscore_app system_adbd_prop file { read open getattr map }
-allow gmscore_app adbd_prop file { read open getattr map }
 allow rild vendor_pd_locater_dbg_prop file { read open getattr map }
-allow vendor_poweroffalarm_app default_android_service service_manager { find }
-allow vendor_poweroffalarm_app hyperos_cust_feature_resolve_service service_manager { find }
-allow vendor_systemhelper_app default_android_service service_manager { find }
-allow vendor_systemhelper_app hyperos_cust_feature_resolve_service service_manager { find }
-allow vendor_embmssl_app hyperos_cust_feature_resolve_service service_manager { find }
-allow bpfloader vendor_default_prop file { read open getattr map }
-allow system_app keyguard_config_prop file { read open getattr map }
-allow system_app last_boot_reason_prop file { read open getattr map }
-allow system_server vendor_wfd_sys_debug_prop file { read open getattr map }
-allow system_server vendor_wfd_sys_prop file { read open getattr map }
-allow system_server wifi_hal_prop file { read open getattr map }
-allow system_server default_android_service service_manager { find }
-allow surfaceflinger default_android_service service_manager { find }
-allow surfaceflinger mcd_data_file dir { search }
-allow untrusted_app proc_net file { read open getattr }
-allow untrusted_app proc_net_tcp_udp file { read open getattr }
-allow untrusted_app selinuxfs file { read open getattr }
-allow untrusted_app build_attestation_prop file { read open getattr map }
-allow untrusted_app serialno_prop file { read open getattr map }
+allow system_app default_android_service service_manager { find }
+allow system_app vendor_xiaomi_hardware_micharge_service service_manager { find }
+allow platform_app default_android_service service_manager { find }
+allow platform_app mcd_data_file dir { search }
+allow platform_app migt_file dir { search }
+allow mediaprovider_app default_android_service service_manager { find }
+allow mediaprovider_app hyperos_cust_feature_resolve_service service_manager { find }
+allow vendor_wlc_app hyperos_cust_feature_resolve_service service_manager { find }
+allow rkpdapp hyperos_cust_feature_resolve_service service_manager { find }
+allow vendor_qtelephony data_log_stability_file dir { search }
+allow untrusted_app sysfs_zram file { read open getattr }
+allow untrusted_app data_log_file dir { search }
 "
 
 SEPOLICY_DIR="/data/adb/modules/pitchkernel_tuning"
